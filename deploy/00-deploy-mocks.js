ea5612a624eb898902edb11a0a7eb88e10117429
FerraroSoftware <software.ferraro@gmail.com>
2022-06-16 21:29:05 -0400

    added deployscript for random ipfs, ipfs script to upload images and metadate, updated ids... broken on 02 deploy

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,13 +1,21 @@
-const { network } = require("hardhat");
-const { DECIMALS, INITIAL_PRICE } = require("../helper-hardhat-config");
+const { getNamedAccounts, deployments, network, ethers } = require("hardhat");
+const { developmentChains } = require("../helper-hardhat-config");
 
-const BASE_FEE = "250000000000000000"; // 0.25 is this the premium in LINK?
+// const BASE_FEE = "250000000000000000"; // 0.25 is this the premium in LINK?
+const BASE_FEE = ethers.utils.parseEther("0.2"); // 0.25 is this the premium in LINK?
 const GAS_PRICE_LINK = 1e9; // link per gas, is this the gas lane? // 0.000000001 LINK per gas
+
+// Chainlink nodes pay the gas fees to give us randomness and do external execution
+// price of requests change based on the price of gas
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
   const chainId = network.config.chainId;
+
+  //  if (developmentChains.includes(network.name)){
+  //  }
+
   // If we are on a local development network, we need to deploy mocks!
   if (chainId == 31337) {
     log("Local network detected! Deploying mocks...");
@@ -16,21 +24,16 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
       log: true,
       args: [BASE_FEE, GAS_PRICE_LINK],
     });
-    await deploy("MockV3Aggregator", {
-      from: deployer,
-      log: true,
-      args: [DECIMALS, INITIAL_PRICE],
-    });
 
     log("Mocks Deployed!");
-    log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    log("----------------------------------------------------------");
     log(
       "You are deploying to a local network, you'll need a local network running to interact"
     );
     log(
       "Please run `yarn hardhat console --network localhost` to interact with the deployed smart contracts!"
     );
-    log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    log("----------------------------------------------------------");
   }
 };
-module.exports.tags = ["all", "mocks", "main"];
+module.exports.tags = ["all", "mocks"];
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -7,7 +7,7 @@ const networkConfig = {
   },
   31337: {
     name: "localhost",
-    subscriptionId: "588",
+    subscriptionId: "6500",
     gasLane:
       "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
     keepersUpdateInterval: "30",
@@ -16,7 +16,7 @@ const networkConfig = {
   },
   4: {
     name: "rinkeby",
-    subscriptionId: "588",
+    subscriptionId: "6500",
     gasLane:
       "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
     keepersUpdateInterval: "30",
